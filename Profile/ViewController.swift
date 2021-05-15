a030d20cdb4cd8a50c5abd6f82e5972a5e812eaa
kubota yuno <im.kbxynx@2106gmail.com>
2021-05-15 22:49:38 +0900

    プロフィールアプリの完成

--- Profile/ViewController.swift
+++ Profile/ViewController.swift
@@ -8,10 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var profileImageView: UIImageView!
+    
+    @IBOutlet var profileCommentLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        profileImageView.isHidden = true
+        
+        profileCommentLabel.isHidden = true
         // Do any additional setup after loading the view.
+    }
+    @IBAction func tapButton1(){
+        
+        profileImageView.isHidden = false
+        
+        profileCommentLabel.isHidden = false
+        
     }
 
 
